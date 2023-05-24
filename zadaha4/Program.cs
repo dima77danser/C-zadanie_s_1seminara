30e31e655cf4534cc003ca6135af23d9fbe9c872
Dmitrii <lukashin1988@inbox.ru>
2023-05-24 19:49:09 +0300

    max iz 3

---
+++ zadaha4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("ВВедите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ВВедите число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ВВедите число:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num;
+
+if (num2 > max)
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+
+Console.Write("max:");
+Console.WriteLine(max);
